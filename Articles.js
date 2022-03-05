54a7bd16cd415f8f19b91171e917cde76a36d200
Paul <menut34@gmail.com>
2022-03-05 10:13:04 +0100

    add Article class

---
+++ Articles.js
@@ -0,0 +1,11 @@
+class Articles {
+    constructor(Name,Price,Shop,Image) {
+        this.Name = Name;
+        this.Price = Price;
+        this.Shop = Shop;
+        this.Image = Image;
+
+    }
+}
+
+module.exports = Articles;
